3cc11273626188bd2916c8b59315a06499f95c81
Shasha Sami <shasha.sami91199@gmail.com>
2020-08-06 23:36:10 +0530

    Linear Search

--- LinearSearch.c
+++ LinearSearch.c
@@ -11,7 +11,7 @@ int search(int arr[], int n, int x)
   
 int main(void) 
 { 
-    int arr[] = { 2, 3, 4, 10, 40 }; 
+    int arr[] = {2, 3, 4, 10, 40 }; 
     int x = 10; 
     int n = sizeof(arr) / sizeof(arr[0]); 
     int result = search(arr, n, x); 
